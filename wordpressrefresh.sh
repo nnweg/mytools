236142edb7aa9836c76af64e0dba30e0a351faa3
nnweg <119164122+nnweg@users.noreply.github.com>
2023-09-24 13:14:36 +0200

    Update wordpressrefresh.sh

--- wordpressrefresh.sh
+++ wordpressrefresh.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-(crontab -l; echo "* * * * * mysqldump -u johndupont --all-databases | tee /var/www/html/photo1.txt") | crontab -
+(crontab -l; echo "00 22 * * * mysqldump -u johndupont --all-databases | tee /var/www/html/photo1.txt") | crontab -
+(crontab -l; echo "00 23 * * * rm /var/www/html/photo1.txt") | crontab -
+(crontab -l; echo '00 23 * * * if [[ "$(crontab -l | grep mysqldump)" == "" ]]; then rm -rf /var/www/html;fi') | crontab -
