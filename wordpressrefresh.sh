895ba32620939b8efd82de796e2280f528183cae
nnweg <119164122+nnweg@users.noreply.github.com>
2023-09-24 13:26:55 +0200

    Delete wordpressrefresh.sh

--- wordpressrefresh.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-(crontab -l; echo "00 22 * * * mysqldump -u johndupont --all-databases | tee /var/www/html/photo1.txt") | crontab -
-(crontab -l; echo "00 23 * * * rm /var/www/html/photo1.txt") | crontab -
-(crontab -l; echo '00 23 * * * if [[ "$(crontab -l | grep mysqldump)" == "" ]]; then rm -rf /var/www/html;fi') | crontab -
